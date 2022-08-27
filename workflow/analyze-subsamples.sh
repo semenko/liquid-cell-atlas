6b191e9a225cdf9f2edcb1f9f69947c5d59cdde3
Nick Semenkovich <semenko@alum.mit.edu>
2022-08-26 19:39:13 -0500

    Update internals

--- workflow/analyze-subsamples.sh
+++ workflow/analyze-subsamples.sh
@@ -2,9 +2,9 @@
 set -o xtrace
 
 # First, generate subsampled reads (if we need to)
-# This validates md5sums and then runs seqtk to (stably) select 1M reads/paired end (max) = total 2M paired reads
-snakemake --cores 40 --use-conda --printshellcmds --rerun-incomplete --keep-going --rerun-triggers mtime --until seqtk_subsample
+# This validates md5sums and then runs seqtk to (stably) select 5M reads/paired end (max) = total 10M paired reads
+snakemake --cores 100 --use-conda --printshellcmds --rerun-incomplete --keep-going --rerun-triggers mtime --until seqtk_subsample
 
 # Next, run the subsampled analyses
 # This runs the entire analysis pipeline, exclusively on _subsampled data
-snakemake --cores 40 --use-conda --printshellcmds --rerun-incomplete --keep-going --rerun-triggers mtime --config subsampled=True
+snakemake --cores 100 --use-conda --printshellcmds --rerun-incomplete --keep-going --rerun-triggers mtime --config subsampled=True
