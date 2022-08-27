af113af473134098427cfb1debad6f3296de824d
Nick Semenkovich <semenko@alum.mit.edu>
2022-08-27 07:20:44 -0500

    Does this touch order change timing?

--- workflow/analyze-subsamples.sh
+++ workflow/analyze-subsamples.sh
@@ -3,6 +3,7 @@ set -o xtrace
 
 # First, generate subsampled reads (if we need to)
 # This validates md5sums and then runs seqtk to (stably) select 5M reads/paired end (max) = total 10M paired reads
+# This runs about 8 jobs in parallel, and takes roughly 8 hours to complete for melanoma (150 samples)
 snakemake --cores 100 --use-conda --printshellcmds --rerun-incomplete --keep-going --rerun-triggers mtime --until seqtk_subsample
 
 # Next, run the subsampled analyses
